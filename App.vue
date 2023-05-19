17fcf7edf809480bf8a820937e13f1c47e26cdb4
shaunBoahCodes <131640457+shaunBoahCodes@users.noreply.github.com>
2023-05-19 21:21:21 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,20 @@
+<template>
+  <Matrix v-if="store.showIntro"></Matrix>
+  <Home v-if="!store.showIntro"></Home>
+</template>
+
+<script setup>
+import UserInput from '@/components/textbox/UserInput'
+import SelfType from '@/components/SelfType'
+import Matrix from '@/components/Matrix'
+import Home from '@/components/Home'
+import { matrixStore } from '@/store/matrixStore.js'
+const store = matrixStore()
+import { ref } from 'vue'
+
+
+</script>
+
+<style>
+
+</style>
